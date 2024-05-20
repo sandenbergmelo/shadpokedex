defa8477c24579dbe688d609257206f109a5b78e
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2024-05-20 17:19:03 -0300

    Refactor Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,13 +11,14 @@ export function Header({ searchTermState, handleSearch }: HeaderProps) {
       <div className='mb-4'>
         <h1 className='text-center font-PokemonSolid text-6xl'>Pokédex</h1>
       </div>
-      <div className='container mx-auto'>
+      <div className='container'>
         <Input
           type='search'
           className='ml-4 h-16 w-min text-xl'
           placeholder='Pesquisar Pokémon 🔍'
           value={searchTermState}
           onChange={handleSearch}
+          aria-label='Pesquisar Pokémon'
         />
       </div>
     </header>
